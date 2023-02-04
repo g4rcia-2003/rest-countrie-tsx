9fe1aff34bac718f2686e4a141457574d4d78cc7
santiago23-2003 <santiag636@gmail.com>
2023-02-03 20:18:44 -0500

    App End

--- src/pages/countrie/Countrie.tsx
+++ src/pages/countrie/Countrie.tsx
@@ -45,85 +45,89 @@ const Countrie = () => {
 
 	return (
 		<LayoutInfoCountrie>
-			{data !== undefined && viewData ? (
-				<>
-					<Link to={"/"}>
-						<Button>
-							<BackIcon />
-							<p>Back</p>
-						</Button>
-					</Link>
-					<div className='infoCountrie'>
-						<ImageCountrie>
-							<img src={data?.flags.png} alt={`${data?.flags.alt}`} />
-						</ImageCountrie>
-						<InfoCountrie>
-							<div className='data'>
-								<div className='one'>
-									<h2>{data?.name.common}</h2>
-									<p>
-										<span>Native Name:</span> {data?.name.official}
-									</p>
-									<p>
-										<span>Population:</span>{" "}
-										{data?.population.toLocaleString("en")}
-									</p>
-									<p>
-										<span>Region:</span> {data?.region}
-									</p>
-									<p>
-										<span>Sub Region:</span> {data?.subregion}
-									</p>
-									<p>
-										<span>Capital:</span> {data?.capital}
-									</p>
-								</div>
-								<div className='two'>
-									<p>
-										<span>Top Level Domain:</span> {data?.tld[0]}
-									</p>
-									<p>
-										<span>Currencies:</span>{" "}
-										{currencies?.map((x, i) => (
-											<span key={i}>
-												{x as unknown as string}
-												{i === currencies.length && ","}
-											</span>
-										))}
-									</p>
-									<div className='languages'>
+			{data !== undefined ? (
+				viewData ? (
+					<>
+						<Link to={"/"}>
+							<Button>
+								<BackIcon />
+								<p>Back</p>
+							</Button>
+						</Link>
+						<div className='infoCountrie'>
+							<ImageCountrie>
+								<img src={data?.flags.png} alt={`${data?.flags.alt}`} />
+							</ImageCountrie>
+							<InfoCountrie>
+								<div className='data'>
+									<div className='one'>
+										<h2>{data?.name.common}</h2>
 										<p>
-											<span>Languages:</span>
-											{language?.map((x, i) => (
+											<span>Native Name:</span> {data?.name.official}
+										</p>
+										<p>
+											<span>Population:</span>{" "}
+											{data?.population.toLocaleString("en")}
+										</p>
+										<p>
+											<span>Region:</span> {data?.region}
+										</p>
+										<p>
+											<span>Sub Region:</span> {data?.subregion}
+										</p>
+										<p>
+											<span>Capital:</span> {data?.capital}
+										</p>
+									</div>
+									<div className='two'>
+										<p>
+											<span>Top Level Domain:</span> {data?.tld[0]}
+										</p>
+										<p>
+											<span>Currencies:</span>{" "}
+											{currencies?.map((x, i) => (
 												<span key={i}>
-													{" "}
-													{x as unknown as string}{" "}
-													{i === language.length && ","}{" "}
+													{x as unknown as string}
+													{i === currencies.length && ","}
 												</span>
 											))}
 										</p>
+										<div className='languages'>
+											<p>
+												<span>Languages:</span>
+												{language?.map((x, i) => (
+													<span key={i}>
+														{" "}
+														{x as unknown as string}{" "}
+														{i === language.length && ","}{" "}
+													</span>
+												))}
+											</p>
+										</div>
 									</div>
 								</div>
-							</div>
-							<div className='border-countries'>
-								<h2>Border Countries:</h2>
-								<div className='countries'>
-									{data?.borders === undefined ? (
-										<span> Does Not Have</span>
-									) : (
-										data?.borders.map(i => (
-											<span className='countrie' key={i}>
-												{i}
-											</span>
-										))
-									)}
+								<div className='border-countries'>
+									<h2>Border Countries:</h2>
+									<div className='countries'>
+										{data?.borders === undefined ? (
+											<span> Does Not Have</span>
+										) : (
+											data?.borders.map(i => (
+												<span className='countrie' key={i}>
+													{i}
+												</span>
+											))
+										)}
+									</div>
 								</div>
-							</div>
-						</InfoCountrie>
-					</div>
-				</>
+							</InfoCountrie>
+						</div>
+					</>
+				) : (
+					<Loading />
+				)
 			) : (
-				<Loading />
+				<p>Error</p>
 			)}
 		</LayoutInfoCountrie>
 	);
